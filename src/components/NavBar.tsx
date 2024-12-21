671dcd2b804520fc205ffeed278b0cdb4889aca8
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-21 09:14:08 +0100

    chat-app udated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
     return (
-        <div>
-            <div className="containerWrap navbar bg-neutral text-neutral-content">
+        <div className="navbar bg-neutral text-neutral-content">
+            <div className="containerWrap">
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
         </div>

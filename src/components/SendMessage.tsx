43ac5bab8a876a2de23f812d2104a41743339854
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-21 09:47:16 +0100

    chat-app updated

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -3,14 +3,18 @@ import { useState } from "react"
 const SendMessage = () => {
     const [value,setValue] = useState("")
 
-    console.log(value);
+    const handleSendMessage = (e:any) =>{
+        e.preventDefault()
+        console.log(value);
+        setValue("")
+    }
     
   return (
     <div className="bg-gray-200 fixed  bottom-0 w-full py-10 shadow-lg">
-        <form className="containerWrap flex">
+        <form onSubmit={handleSendMessage} className="containerWrap flex">
             <input value={value} onChange={e => setValue(e.target.value)} className="input w-full focus:outline-none
             bg-gray-100 rounded-r-none" type="text" />
-            <button className="w-auto bg-gray-500 text-white 
+            <button type="submit" className="w-auto bg-gray-500 text-white 
             rounded-r-lg px-5 text-sm">Send</button>
         </form>
     </div>
